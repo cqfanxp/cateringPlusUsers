700a1238cc6b09a6432a703226dd93656bfa1968
火爆私厨 <huobaosichu@huobaosichudeimac.local>
2016-07-09 10:10:25 +0800

    构建项目框架

---
+++ cateringPlusUsers/Utility/Common/PrefixHeader.h
@@ -0,0 +1,36 @@
+//
+//  PrefixHeader.h
+//  cateringPlusUsers
+//
+//  Created by 火爆私厨 on 16/7/5.
+//  Copyright © 2016年 餐+. All rights reserved.
+//
+
+//**************调试和发布版本之间的设置*****************
+
+#ifdef DEBUG //调试模式--模拟器
+
+#define HMLog(...) NSLog(__VA_ARGS__)  //公司自定义打印
+
+#else //发布模式 RELEASE--真机
+
+#define HMLog(...)  //发布版本下取消自定义打印，自定义打印不起作用
+
+#endif
+
+//**************所有objective-c文件共享的头文件*****************
+
+#ifdef __OBJC__  //所有objective-c文件共享的头文件
+
+#import "UIView+Frame.h"
+#import <UIKit/UIKit.h>
+
+#define kScreenSize [UIScreen mainScreen].bounds
+#define kScreenWidth kScreenSize.size.width
+#define kScreenHeight kScreenSize.size.height
+
+#define kTabBarHeight 64
+#define kTitleFont [UIFont systemFontOfSize:15]
+#define kDetailFont [UIFont systemFontOfSize:13]
+
+#endif
